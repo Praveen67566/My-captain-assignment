d41fa8f4fbef56827d220d3dfb55fe4e3aaabb95
Praveen67566 <97364496+Praveen67566@users.noreply.github.com>
2022-05-27 13:22:03 +0530

    Update lets_code_ds.py

--- lets_code_ds.py
+++ lets_code_ds.py
@@ -5,19 +5,11 @@ def cal_areaofcir(radius):
     return math.pi * radius * radius
 radius = float(input("Enter the radius of circle : "))
 area = cal_areaofcir(radius)
-print(f"The area of the circle with radius {radius} is: " + str(area))
+print(f"The area of the circle with radius {radius} is: {area}")
 
 # Your second task now is to write a Python program to accept a filename from the user and print the extension of that.
 
-fname = input("Enter the name of file : ")
+filename = input("Enter the name of file : ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
 
-if(fname.endswith(".py")):
-    print("The extension of the file is : python")
-if(fname.endswith(".c")):
-    print("The extension of the file is : c")
-if(fname.endswith(".html")):
-    print("The extension of the file is : html")
-if(fname.endswith(".text")):
-    print("The extension of the file is : text")
-if(fname.endswith(".js")):
-    print("The extension of the file is : javascript")
